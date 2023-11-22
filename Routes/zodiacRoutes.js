ebee77856886cbcbc95353b329e77648f0bf13da
Shivam Garala <shivam@phpdots.com>
2023-11-22 23:24:45 +0530

    authmiddleware added

--- Routes/zodiacRoutes.js
+++ Routes/zodiacRoutes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const Router = express.Router();
 const zodiacController = require('../http/Controllers/zodiacController');
 const fileUpload = require('express-fileupload');
+const authMiddleware = require('../http/middlewares/authMiddleware');
 
-Router.post('/create', fileUpload(), zodiacController().addZodiac);
+Router.post('/create', authMiddleware, fileUpload(), zodiacController().addZodiac);
 
-Router.get('/read',  zodiacController().readZodiac);
+Router.get('/read', authMiddleware,  zodiacController().readZodiac);
 
-Router.put('/update/:id', fileUpload(), zodiacController().updateZodiac);
+Router.put('/update/:id', authMiddleware, fileUpload(), zodiacController().updateZodiac);
 
-Router.delete('/delete/:id',  zodiacController().deleteZodiac);
+Router.delete('/delete/:id', authMiddleware,  zodiacController().deleteZodiac);
 
 module.exports = Router;
