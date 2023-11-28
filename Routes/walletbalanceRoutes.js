5aa27f423b88fe73ee4aad189748e0d95ebcfd8d
Vishal Nakiya <vishal@phpdots.com>
2023-11-28 22:37:26 +0530

    small commit

--- Routes/walletbalanceRoutes.js
+++ Routes/walletbalanceRoutes.js
@@ -3,7 +3,11 @@ const Router = express.Router();
 const WalletBalanceController = require('../http/Controllers/WalletBalanceController');
 const fileUpload = require('express-fileupload');
 const authMiddleware = require('../http/middlewares/authMiddleware');
+const { check } = require('express-validator');
 
-Router.post('/amount/request', authMiddleware, WalletBalanceController().AmountRequest);
+Router.post('/amount/request', authMiddleware, [
+    check("user_id").exists().withMessage("Enter user_id"),
+],
+WalletBalanceController().AmountRequest);
 
 module.exports = Router;
--- http/Controllers/WalletBalanceController.js
+++ http/Controllers/WalletBalanceController.js
@@ -3,7 +3,7 @@ const WalletBalance = require("../../Models/WalletBalance");
 const jwt = require("jsonwebtoken");
 const bcrypt = require('bcrypt');
 
-const BalanceController = () => {
+const WalletBalanceController = () => {
     return {
         AmountRequest: async (req, res) => {
             try {
@@ -31,6 +31,7 @@ const BalanceController = () => {
                     debit_credit: 1,
                     amount: req.body.amount,
                     running_balance: NewBalance + +req.body.amount,
+                    comment: req.body.comment,
                 }
 
                 const balance = await WalletBalance.create(data);
@@ -41,32 +42,32 @@ const BalanceController = () => {
                 res.status(500).json({ success: false, message: "Internal Server error", });
             }
         },
-        // ReadAmountrequest: async (req, res) => {
-        //     try {
-        //         const mydata = await Balance.findOne({
-        //             where: {
-        //                 deleted_at: null,
-        //             },
-        //             attributes: {
-        //                 exclude: ["deleted_at", "auth_token", "refresh_token"],
-        //             },
-        //         })
-        //         if (!mydata) return res.status(400).json({
-        //             success: false,
-        //             message: "No user data found!",
-        //         });;
-        //         //FINALLY, Sending data in response
-        //         res.status(200).json({
-        //             success: true,
-        //             message: "User data fetch succesfully",
-        //             data: mydata,
-        //         })
-        //     } catch (error) {
-        //         console.log(error);
-        //         res.status(500).json({ success: false, message: "Internal Server error", });
-        //     }
-        // },
+        ReadAmountrequest: async (req, res) => {
+            try {
+                const mydata = await Balance.findOne({
+                    where: {
+                        deleted_at: null,
+                    },
+                    attributes: {
+                        exclude: ["deleted_at", "auth_token", "refresh_token"],
+                    },
+                })
+                if (!mydata) return res.status(400).json({
+                    success: false,
+                    message: "No user data found!",
+                });;
+                //FINALLY, Sending data in response
+                res.status(200).json({
+                    success: true,
+                    message: "User data fetch succesfully",
+                    data: mydata,
+                })
+            } catch (error) {
+                console.log(error);
+                res.status(500).json({ success: false, message: "Internal Server error", });
+            }
+        },
     }
 };
 
-module.exports = BalanceController
+module.exports = WalletBalanceController
